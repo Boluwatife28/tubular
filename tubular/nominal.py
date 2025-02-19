51e8ff615381bc7be7471f775eb88c3b6f20dffd
Boluwatife Ajibade <107062550+Boluwatife28@users.noreply.github.com>
2025-02-19 14:05:00 +0000

    setting up draft pr

--- tubular/nominal.py
+++ tubular/nominal.py
@@ -1087,6 +1087,9 @@ class OneHotEncodingTransformer(
         Names of columns to transform. If the default of None is supplied all object and category
         columns in X are used.
 
+    values: list of strings or None, default = None
+        Optional parameter to select specific columns to be transformed. If it is None, all categorical columns will be encoded.
+
     separator : str
         Used to create dummy column names, the name will take
         the format [categorical feature][separator][category level]
@@ -1123,6 +1126,7 @@ class OneHotEncodingTransformer(
     def __init__(
         self,
         columns: str | list[str] | None = None,
+        values: list[str] | None = None,
         separator: str = "_",
         drop_original: bool = False,
         copy: bool | None = None,
@@ -1137,6 +1141,7 @@ class OneHotEncodingTransformer(
             **kwargs,
         )
 
+        self.values = values
         self.set_drop_original_column(drop_original)
         self.check_and_set_separator_column(separator)
 
@@ -1183,6 +1188,10 @@ class OneHotEncodingTransformer(
 
             # for consistency
             levels_list.sort()
+
+            # filter levels_list using 'values' (if provided)
+            if self.values is not None and c not in self.values:
+                continue  # skips columns not in 'values'
 
             self.categories_[c] = levels_list
 
